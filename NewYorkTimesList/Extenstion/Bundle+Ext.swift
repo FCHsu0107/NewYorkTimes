563ef7374b0288d3dbc8abd147fc4b08718d1135
Jacuqueline_Hsu <hsufuchiung@gmail.com>
2021-09-14 19:58:06 +0100

    Add the api key in keys.plist file and ignore this file in git

--- NewYorkTimesList/Extenstion/Bundle+Ext.swift
+++ NewYorkTimesList/Extenstion/Bundle+Ext.swift
@@ -7,10 +7,23 @@
 
 import Foundation
 
+enum APIKeyType: String {
+    
+    case client = "clientKey"
+}
+
 extension Bundle {
     
     static func valueForString(_ key: String) -> String? {
         
         return Bundle.main.infoDictionary?[key] as? String
+    }
+    
+    static func apiKey(_ type: APIKeyType) -> String? {
+        
+        guard let path = Bundle.main.path(forResource: "keys", ofType: "plist"),
+              let keys = NSDictionary(contentsOfFile: path) else { return nil }
+        
+        return keys[type.rawValue] as? String
     }
 }
