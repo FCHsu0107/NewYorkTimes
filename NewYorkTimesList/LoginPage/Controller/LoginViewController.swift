872b9bc8d658fb68547ecc9d911367f6c9eec5b9
Jacuqueline_Hsu <hsufuchiung@gmail.com>
2021-09-17 21:29:10 +0100

    Add the layout for log in page

--- NewYorkTimesList/LoginPage/Controller/LoginViewController.swift
+++ NewYorkTimesList/LoginPage/Controller/LoginViewController.swift
@@ -11,6 +11,47 @@ class LoginViewController: UIViewController {
     
     typealias ViewModel = LoginPageViewModelInput & LoginPageViewModelOutput
     
+    var userNameTextField: UITextField = {
+        
+        let textField = UITextField()
+        
+        textField.placeholder = "User name"
+        
+        textField.borderStyle = .roundedRect
+
+        return textField
+        
+    }()
+    
+    var passwordTextField: UITextField = {
+        
+        let textField = UITextField()
+        
+        textField.placeholder = "Password"
+        
+        textField.borderStyle = .roundedRect
+
+        return textField
+        
+    }()
+    
+    lazy var loginButton: UIButton = {
+        
+        let btn = UIButton()
+        
+        btn.addTarget(self, action: #selector(loginBtnDidClick(_:)), for: .touchUpInside)
+        
+        btn.setTitle("Log in", for: .normal)
+        
+        btn.setTitleColor(FontType.button.color, for: .normal)
+        
+        btn.backgroundColor = TT.badgeBGColor
+        
+        btn.layer.cornerRadius = TT.badgeCornerRadius
+        
+        return btn
+    }()
+    
     var viewModel: ViewModel
     
     init() {
@@ -30,13 +71,52 @@ class LoginViewController: UIViewController {
         setUpUI()
         
         viewModelDataBinding()
-        
     }
     
     private func setUpUI() {
         
+        view.backgroundColor = .white
+        
+        view.addSubview(userNameTextField)
+        
+        userNameTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            userNameTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            userNameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: TT.sideInset),
+            userNameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -TT.sideInset)
+        ])
+        
+        view.addSubview(passwordTextField)
+        
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            passwordTextField.topAnchor.constraint(equalTo: userNameTextField.bottomAnchor, constant: TT.verticalInset),
+            passwordTextField.leadingAnchor.constraint(equalTo: userNameTextField.leadingAnchor),
+            passwordTextField.trailingAnchor.constraint(equalTo: userNameTextField.trailingAnchor)
+        ])
+        
+        view.addSubview(loginButton)
+        
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: TT.verticalInset),
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginButton.widthAnchor.constraint(equalToConstant: 120),
+            loginButton.heightAnchor.constraint(equalToConstant: 40)
+        ])
     }
     
+    @objc func loginBtnDidClick(_ sender: UIButton) {
+        
+        guard let userName = userNameTextField.text, let password = passwordTextField.text else {
+            return // TODO pop alert
+        }
+        
+        viewModel.login(userName: userName, password: password)
+    }
 }
 
 extension LoginViewController {
@@ -52,5 +132,4 @@ extension LoginViewController {
     func switchToBookListPage() {
         // TODO
     }
-    
 }
